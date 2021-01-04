e0af8b3e4b08c0eb99afc2c528f4cd05bf0b2b0b
zaidaanhilmi <76931298+zaidaanhilmi@users.noreply.github.com>
2021-01-04 11:40:17 +0700

    templates
    upload templates

---
+++ my.portofolio/footer.php
@@ -0,0 +1,25 @@
+﻿<?php $shortname = "slidemag"; ?>
+<?php //if (!is_home() && !is_front_page() && !is_archive()) { ?>
+<div class="footer_top_cont">
+	<div class="container">
+		
+	</div> <!-- //container -->
+</div> <!-- //footer_top_cont -->
+<div class="footer_copyright_cont">
+<div class="footer_copyright">
+	<div class="container">
+		<!-- DELETE THIS -->
+					
+		<footer>
+	 © 2019 All Rights Reserved. Powered by <a href="https://dessign.net/best-free-wordpress-themes/">WordPress</a> 
+	
+	
+	</footer>
+		<div class="clear"></div>
+	</div><!--//container-->
+</div><!--//footer_copyright-->
+</div><!--//footer_copyright_cont-->
+<?php //} ?>
+<?php wp_footer(); ?>
+</body>
+</html>
